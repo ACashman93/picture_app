cced42547ca4dc1e59af9489748519754f78c0be
Andrew Cashman <andrewcashman@Andrews-MacBook-Pro.local>
2018-04-10 12:40:12 -0700

    Final update to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,60 +18,60 @@ Type.create!(title: "Family")
 
 puts "Family category created"
 
-Type.create!(title: "Events")
+Type.create!(title: "Event")
 
 puts "Events category created"
 
-Type.create!(title: "Holidays")
+Type.create!(title: "Holiday")
 
 puts "Holidays category created"
 
-12.times do |picture|
+20.times do |picture|
   Picture.create!(image: "https://placehold.it/300x300",
-                  description: "Andrew #{picture} description. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                  date: Date.today,
+                  description: "Andrew #{picture} description. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                  date: Date.today - 365,
                   type_id: Type.first!.id
                   )
 end
 
-puts "12 Andrew pictures created"
+puts "20 Andrew pictures created"
 
-12.times do |picture|
+20.times do |picture|
   Picture.create!(image: "https://placehold.it/300x300",
-                  description: "Jacob #{picture} description. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                  date: Date.today - 1,
+                  description: "Jacob #{picture} description. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                  date: Date.today - 730,
                   type_id: Type.second!.id
                   )
 end
 
-puts "12 Jacob pictures created"
+puts "20 Jacob pictures created"
 
-12.times do |picture|
+20.times do |picture|
   Picture.create!(image: "https://placehold.it/300x300",
-                  description: "My Family #{picture} description. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                  date: Date.today - 2,
+                  description: "Family #{picture} description. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                  date: Date.today - 1095,
                   type_id: Type.third!.id
                   )
 end
 
-puts "12 Family pictures created"
+puts "20 Family pictures created"
 
-12.times do |picture|
+20.times do |picture|
   Picture.create!(image: "https://placehold.it/300x300",
-                  description: "My Events #{picture} description. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                  date: Date.today - 3,
+                  description: "Events #{picture} description. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                  date: Date.today - 1460,
                   type_id: Type.fourth!.id
                   )
 end
 
-puts "12 Events pictures created"
+puts "20 Events pictures created"
 
-12.times do |picture|
+20.times do |picture|
   Picture.create!(image: "https://placehold.it/300x300",
-                  description: "My holidays #{picture} description. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                  date: Date.today - 4,
+                  description: "Holidays #{picture} description. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                  date: Date.today - 1825,
                   type_id: Type.fifth!.id
                   )
 end
 
-puts "12 Holidays pictures created"
+puts "20 Holidays pictures created"
